29c53d9e7adbd132841824dfb4ec845d0edf9a07
vipghn2003 <47717603+vipghn2003@users.noreply.github.com>
2022-03-21 21:44:58 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,16 +25,16 @@ bool Init_All()
 	}
 	window = SDL_CreateWindow("Astro", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (window == nullptr)
-    {
-        logSDLError(cout, "CreateWindow", true);
-        return false;
-    }
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == NULL)
-    {
-        cout << "Failed to create window: " << SDL_GetError() << endl;
-        return false;
-    }
+    	{
+       		logSDLError(cout, "CreateWindow", true);
+        	return false;
+    	}
+    	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    	if (renderer == NULL)
+    	{
+        	cout << "Failed to create window: " << SDL_GetError() << endl;
+        	return false;
+    	}
 	return true;
 }
 
