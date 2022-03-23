dc6db71733b9cdf129025d0be38856f4c279f8e5
vipghn2003 <47717603+vipghn2003@users.noreply.github.com>
2022-03-23 16:36:24 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,6 +38,56 @@ bool Init_All()
 	return true;
 }
 
+bool Load_Asteroids(const char* file_path)
+{
+    Asteroid* cur_Asteroid = new Asteroid;
+    if (!cur_Asteroid->Load_Image(file_path, renderer))
+    {
+        cout << "Image Error: " << endl;
+        return false;
+    }
+	return true;
+}
+
+bool Load()
+{
+	font_32 = TTF_OpenFont("Game_BG/Font.ttf", 32);
+	font_24 = TTF_OpenFont("Game_BG/Font.ttf", 24);
+	if (!Background_logo.Load_Image("Game_BG/Background_logo.png", renderer))
+	{
+		cout << "Background Logo error!" << endl;
+		return false;
+	}
+	if (!Background_menu.Load_Image("Game_BG/Background_menu.png", renderer))
+	{
+		cout << "Background menu error!" << endl;
+		return false;
+	}
+	if (!Instructions.Load_Image("Game_BG/Instructions.png", renderer))
+	{
+		cout << "Instruction background error!" << endl;
+		return false;
+	}
+	if (!Arrow_Selected.Load_Image("Game_BG/Arrow.png", renderer))
+	{
+		cout << "Arrow selected error!" << endl;
+		return false;
+	}
+	if (!Playing_Background.Load_Image("Game_BG/Background_main.png", renderer))
+	{
+		cout << "Background Error!" << endl;
+		return false;
+	}
+	if (!Player_Spaceship.Load_Image("Game_BG/Spaceship.png", renderer))
+	{
+		cout << "Image Error: " << endl;
+		return false;
+	}
+	if (!Load_Asteroids("Game_BG/Big_Asteroid.png")) return false;
+	if (!Load_Asteroids("Game_BG/Medium_Asteroid.png")) return false;
+	if (!Load_Asteroids("Game_BG/Small_Asteroid.png")) return false;
+}
+
 void close()
 {
 	SDL_DestroyRenderer(renderer);
